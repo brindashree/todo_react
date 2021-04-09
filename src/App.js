e6bfd5cc610d367f412a8f22371e2407e63c0e73
Brindashree C B <brindashree@users.noreply.github.com>
2021-04-09 19:19:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
-
+import TextField from '@material-ui/core/TextField';
+import { useEffect, useState } from "react";
+import { Button } from '@material-ui/core';
+import { db } from './firebase_config';
+import firebase from "firebase";
+import TodoListItem from './todo';
 function App() {
+  const [todos,setTodos] = useState([])
+  const [todoInput, setTodoInput] = useState('');
+
+  useEffect(() => {
+    getTodos();
+  },[])
+  function getTodos() {
+    db.collection("todos").onSnapshot(function (querySnapshot) {
+      setTodos(querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        todo: doc.data().todo,
+        inprogress: doc.data().inprogress
+      })));
+    });
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    db.collection("todos").add(
+      {
+        inprogress: true,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        todo: todoInput,
+      }
+    );
+    setTodoInput("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+     >
+      <div
+        style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+          alignItems: "center",
+        width:"100%",
+      }}>
+      <h1> TODO APP</h1>
+        <form>
+          <TextField id="outlined-basic" label="Write a TODO" variant="outlined"
+          value={todoInput}
+          onChange={(e) => {
+            setTodoInput(e.target.value)
+            
+          }}
+          style={{
+          width:"90vw", maxWidth:"500px"
+          }} />
+          <Button type="submit" variant="contained" color="primary" onClick={addTodo}
+            style={{
+            display:"none"
+          }}>
+  Primary
+</Button>
+        </form>
+        <div  style={{
+          width:"90vw", maxWidth:"500px",marginTop:"24px"
+          }}>
+        {todos.map((todo) => (
+          <TodoListItem todo={todo.todo} inprogress={todo.inprogress} id={todo.id}/>
+        ))}</div>
+        </div>
     </div>
   );
 }
