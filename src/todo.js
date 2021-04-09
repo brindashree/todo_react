4d33aa1858b22d5e38e29a3e81a88883eea09932
Brindashree C B <43085707+brindashree@users.noreply.github.com>
2021-04-09 20:22:33 +0530

    Update todo.js

--- src/todo.js
+++ src/todo.js
@@ -1,6 +1,7 @@
  import { Button, ListItem, ListItemText } from '@material-ui/core'
 import React from 'react'
 import { db } from './firebase_config'
+import "./todo.css"
  
 export default function TodoListItem({ todo, inprogress, id }) {
      
@@ -13,16 +14,17 @@ export default function TodoListItem({ todo, inprogress, id }) {
         db.collection("todos").doc(id).delete();
     }
      return (
-         <div style={{
-             display:"flex"
-         }}>
+         <div className="tile">
              <ListItem>
                  <ListItemText primary={todo} secondary={ inprogress ? "In progress":"Completed"}/>
              </ListItem>
              <Button onClick={toggleInProgress}>
                  { inprogress ? "Done":"Undone"}
              </Button>
-             <Button onClick={deleteTodo}>
+             <Button style={{
+color:"red",
+             }}
+                 onClick={deleteTodo}>
                  X
              </Button>
             
